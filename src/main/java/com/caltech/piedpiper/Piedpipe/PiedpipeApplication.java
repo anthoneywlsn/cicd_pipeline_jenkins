72ff66c9765997bb45d89d0e4348269b0a37f768
anthoneywlsn <anthoneywlsn@yahoo.com>
2021-06-27 12:06:11 -0400

    cool Maven Spring Porject

---
+++ src/main/java/com/caltech/piedpiper/Piedpipe/PiedpipeApplication.java
@@ -0,0 +1,13 @@
+package com.caltech.piedpiper.Piedpipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiedpipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiedpipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/piedpiper/Piedpipe/PiedpipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.piedpiper.Piedpipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiedpipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
